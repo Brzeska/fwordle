1e893008de775c291c157106e1211d85195f6243
Phillip Brandel <pdbrandel@gmail.com>
2026-08-22 19:18:36 -0700

    Added shell script word fetcher

---
+++ fetchWordlist.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#this pulls a putative list of "all valid wordle guesses" from github
+curl -O https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt
