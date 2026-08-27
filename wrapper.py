1dd9bf520b706f3780a16604fdc399b69b84de12
Phillip Brandel <pdbrandel@gmail.com>
2026-08-26 18:34:14 -0700

    fixed wordle # in wrapper for summer, but will be broken by an hour in winter still

--- wrapper.py
+++ wrapper.py
@@ -3,7 +3,7 @@ import time
 with open('raw.txt', 'r') as f:
     lines = list(f.read().splitlines())#f.read() stores words.txt as a string, splitlines() converts that string into a list
 
-t=int(time.time()/86400)-18797
+t=int(time.time()/86400-7/24)-18797
 
 l = len(lines)
 if (l==6) and (lines[5]!='ggggg'):
