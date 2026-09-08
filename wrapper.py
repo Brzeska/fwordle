1fa8124963a455b185488a52877b7e8ffb2176e8
Phillip Brandel <pdbrandel@gmail.com>
2026-09-07 22:11:53 -0700

    made wrapper kind of compatible with both classical solver gen and neural network, need to refactor because confusing

--- wrapper.py
+++ wrapper.py
@@ -1,5 +1,9 @@
 import time
 
+#note: please refactor into two separate files
+#one for solver.py and one for the neural net
+#the if statements are confusing
+
 with open('raw.txt', 'r') as f:
     lines = list(f.read().splitlines())#f.read() stores words.txt as a string, splitlines() converts that string into a list
 
@@ -8,6 +12,10 @@ t=int(time.time()/86400-7/24)-18797
 l = len(lines)
 if (l==6) and (lines[5]!='22222'):
     l = 'X'
+if (l==7) and (lines[5]!='22222'):
+    l = 'X'
+if (lines[len(lines)-1] == '.....') and (l != 'X'):
+    l-=1
 
 out = f"Wordle {t:,} {l}/6*\n\n"
 
